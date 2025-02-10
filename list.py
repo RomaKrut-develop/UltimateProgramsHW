52ab79cff9273fdd02a4cf7008593d14dae77a1a
Roman Braginetz <animatorroma228@gmail.com>
2025-02-10 22:33:43 +0300

    I recompile all project

--- list.py
+++
@@ -1,8 +0,0 @@
-fruits = ["яблоко", "банан", "вишня"]
-first_fruit = fruits[0]  # Индекс 0 - "яблоко"
-second_fruit = fruits[1] # Индекс 1 - "банан"
-last_fruit = fruits[2] # Индекс 2 - "вишня"
-# fruits[3] # Вызовет ошибку IndexError: list index out of range (индекс за пределами списка)
-print(first_fruit) # Выведет: яблоко
-print(second_fruit) # Выведет: банан
-print(last_fruit) # Выведет: вишня
